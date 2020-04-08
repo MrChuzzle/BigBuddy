cf11656fdbf2ce7781d6567c3fcd5cc287f09bd2
Daniel Schreiber <daniel@DS-MBP.local>
2020-04-08 10:20:37 +0200

    Committer: Daniel Schreiber <daniel@geekblog.dev>  On branch master  Changes to be committed: 	modified:   app.js [CHG] app.js now main.js

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Node.js is Up and Running. \o/ *excited*\n');
-}).listen(8080, '127.0.0.1');
+//var http = require('http');
+//http.createServer(function (req, res) {
+//  res.writeHead(200, {'Content-Type': 'text/plain'});
+//  res.end('Node.js is Up and Running. \o/ *excited*\n');
+//}).listen(8080, '127.0.0.1');
+var BigBuddy = require("./discord/BigBuddy").oBigBuddy;
+var http = require("http");
+
+// init big buddy bot
+var oBigBuddy = new BigBuddy(process.argv.slice(2));
+
+buildServer();
+
+function buildServer() {
+    return http.createServer(function (oRequest, oResponse) {
+        // config server
+        oResponse.setHeader("Access-Control-Allow-Origin", "*");
+        oResponse.setHeader("Access-Control-Allow-Credentials", true);
+        oResponse.setHeader("Access-Control-Allow-Methods", "POST");
+        oResponse.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type");
+        oResponse.writeHead(200, {"Content-Type": "application/json"});
+        oResponse.write("Message successfully received!");
+        oResponse.end();
+
+        // handling requests
+        var sMessageData = "";
+
+        oRequest.on("error", oError => {
+            console.log("Error: ", oError);
+        }).on("data", oData => {
+            sMessageData += oData.toString();
+        }).on("end", () => {
+            oBigBuddy.execute(sMessageData);
+        });
+    }).listen(8082);
+}
